092c632ef6aa9e02db6b3d249f44fc8b9f4204ee
ahmedesmail07 <ahmedismail332211@gmail.com>
2023-07-22 21:06:57 +0300

    vercel_deploy_db

--- django_project/settings.py
+++ django_project/settings.py
@@ -120,12 +120,14 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        'OPTIONS': {
+           
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
